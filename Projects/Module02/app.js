31d71ed9e7a2033bcf354f7dd34e0f41392f9b17
Nadir Yusifov <nadiryusifov448@gmail.com>
2023-05-30 10:40:46 +0400

    js

---
+++ Projects/Module02/app.js
@@ -0,0 +1,97 @@
+// Task 1
+// console.log("My name is Nadir")
+
+
+// Task 2
+// let name = "Nadir"
+// console.log(name)
+
+
+// Task 4
+// let bookAmount = 14
+// let bookPrice = 750
+// console.log(bookAmount*bookPrice)
+
+
+// Task 5
+// let kmhSpeed = 75
+// let msSpeed = kmhSpeed*1000/3600
+// console.log(msSpeed)
+
+
+// Task 6
+// let year = prompt("Dogum ilinizi gosterin")
+// console.log(2023-year)
+// let day = 365
+// console.log(365*19)
+
+
+// Task 7
+// let num = 56
+// console.log(num**5)
+// console.log(num%3)
+
+
+// Task 8
+// let firstName = "Nadir"
+// let lastName = "Yusifov"
+// console.log(firstName, lastName + " Sizi tanishligimiza shadam")
+
+
+// Task 9
+// let fullName = prompt ("Adinizi daxil edin")
+// console.log("Salam, Nadir!")
+
+
+// Task 10
+// let fullName = "Nadir Yusifov"
+// console.log(fullName.length)
+
+// Task 11
+// let fullName = prompt("Adinizi daxil edin")
+// let firstSymbol = fullName [0]
+// let secondSymbol = fullName [fullName.indexOf(" ")+1]
+// console.log(firstSymbol,secondSymbol)
+
+
+// Task 12
+// let fullName = prompt("Adinizi daxil edin")
+// let firstSymbol = fullName.slice(0,fullName.indexOf(" "))
+// let secondSymbol = fullName.slice(fullName.indexOf(" ")+1)
+// console.log(secondSymbol,firstSymbol)
+
+
+// Task 13
+// let fullName = prompt("Adinizi daxil edin")
+// console.log("Salam Nadir!")
+
+
+// Task 14
+// let num1 = prompt("Reqem daxil edin!")
+// let num2 = prompt("Reqem daxil edin!")
+// console.log(num1*num2)
+// console.log(Number(num1)+Number(num2))
+// console.log(num1-num2)
+// console.log(num1/num2)
+
+
+// Task 15
+// ?
+
+
+// Task 16 
+// console.log(true+false)
+// console.log("" + 1 + 0)
+// console.log("" - 1 + 0)
+// console.log(6 / "3")
+// console.log(4 + 5 + "px")
+// console.log("4" - 2)
+// console.log("4px" - 2)
+// console.log(7 / 0)
+// console.log("-9" + 5)
+// console.log("-9" - 5)
+// console.log(null + 1)
+// console.log(undefined + 1)
+
+
+// Task 17
