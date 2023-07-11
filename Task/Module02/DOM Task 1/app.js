2b0c4004cfee1ab690a47d9daa19dcfeff7675c7
Nadir Yusifov <nadiryusifov448@gmail.com>
2023-07-11 10:29:03 +0400

    js

---
+++ Task/Module02/DOM Task 1/app.js
@@ -0,0 +1,46 @@
+// Task 1
+// console.log(document.querySelector("h1"));
+
+
+// Task 2
+// === Silmek === 
+// const heading1 = document.querySelector("h1");
+// heading1.remove("h1");
+
+// === Elave etmek ===
+// const newlistItem = document.createElement("li");
+// const ul = document.querySelector("ul");
+// newlistItem.innerHTML = "TITAN RTX";
+// body.append(newlistItem);
+
+
+// Task 3
+// === Bashliq === 
+// const heading = document.querySelector("h1");
+// heading.remove("h1")
+// const newnameItem = document.createElement("h1")
+// const body = document.querySelector("body")
+// newnameItem.innerHTML = "GeForce 20 Series";
+// body.append(newnameItem);
+// body.insertBefore(newnameItem, body.childNodes[0])
+
+// // === Primary Reng === 
+// const listItems = document.querySelectorAll("ul li")
+// listItems.forEach((elem, index, list) => {
+//     elem.style.backgroundColor = "#ebebeb"
+// });
+
+
+// Task 4
+// const primary = document.querySelectorAll(".primary")
+// console.log(document.querySelectorAll(".primary"));
+
+
+// Task 05
+// const listItems = document.querySelectorAll("ul li")
+// listItems.forEach(li => {
+//     li.classList.add("item")
+//     if(!li.classList.contains("primary")){
+//         li.classList.add("secondary")
+//     }
+// })
---
+++ Task/Module02/DOM Task 2/app.js
@@ -0,0 +1,32 @@
+// const buttonact = document.getElementById("btn-convert")
+// const button1 = buttonact.getElementsByClassName(".btn-1")
+// for (let i = 0; i < button.length; i++) {
+//     button1[i].addEventListener("click", function() {
+//         let current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active"
+//     });
+// }
+
+// const button1 = document.querySelector('.btn-1');
+
+// button1.addEventListener('click', () => {
+//   button1.classList.toggle('active');
+// });
+
+// const button2 = document.querySelector('.btn-2');
+
+// button2.addEventListener('click', () => {
+//   button2.classList.toggle('active');
+// });
+// const button3 = document.querySelector('.btn-3');
+
+// button3.addEventListener('click', () => {
+//   button3.classList.toggle('active');
+// });
+// const button4 = document.querySelector('.btn-4');
+
+// button4.addEventListener('click', () => {
+//   button4.classList.toggle('active');
+  
+// });
