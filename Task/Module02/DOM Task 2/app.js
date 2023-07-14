4de49050caf9f214164ffee8f18c106dc6501c6b
Nadir Yusifov <nadiryusifov448@gmail.com>
2023-07-14 22:52:51 +0400

    js

--- Task/Module02/DOM Task 2/app.js
+++ Task/Module02/DOM Task 2/app.js
@@ -1,32 +1,8 @@
-// const buttonact = document.getElementById("btn-convert")
-// const button1 = buttonact.getElementsByClassName(".btn-1")
-// for (let i = 0; i < button.length; i++) {
-//     button1[i].addEventListener("click", function() {
-//         let current = document.getElementsByClassName("active");
-//         current[0].className = current[0].className.replace(" active", "");
-//         this.className += " active"
-//     });
-// }
+const btnlist = document.querySelectorAll("button")
 
-// const button1 = document.querySelector('.btn-1');
-
-// button1.addEventListener('click', () => {
-//   button1.classList.toggle('active');
-// });
-
-// const button2 = document.querySelector('.btn-2');
-
-// button2.addEventListener('click', () => {
-//   button2.classList.toggle('active');
-// });
-// const button3 = document.querySelector('.btn-3');
-
-// button3.addEventListener('click', () => {
-//   button3.classList.toggle('active');
-// });
-// const button4 = document.querySelector('.btn-4');
-
-// button4.addEventListener('click', () => {
-//   button4.classList.toggle('active');
-  
-// });
+btnlist.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btnlist.forEach(btn => btn.classList.remove("active"))
+        btn.classList.add("active");
+    })
+})
