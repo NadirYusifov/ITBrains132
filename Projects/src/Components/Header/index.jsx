07e410b16f3bd32dc88a0c042dbdd47b737922e0
muradorucov <orucowmurad@gmail.com>
2023-08-28 15:50:50 +0400

    feat: menu nav update

--- Projects/src/Components/Header/index.jsx
+++ Projects/src/Components/Header/index.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import ClearIcon from '@mui/icons-material/Clear';
 import MenuIcon from '@mui/icons-material/Menu';
 import "./style.css"
 import { Link } from 'react-router-dom';
 
 function Header() {
-  
-  const [Mobile, setMobile] = useState(false)
+
+  const [Mobile, setMobile] = useState(false);
+  const menu = useRef(null);
+
+  const handleClick = () => {
+    setMobile(!Mobile)
+
+    if (!Mobile) {
+      menu.current.style.top = "0px";
+    } else {
+      menu.current.style.top = "-1000px";
+    }
+  }
 
   return (
     <div className='header-area'>
@@ -16,7 +27,8 @@ function Header() {
             <p>LOGO</p>
           </div>
           <div className='header-right d-flex align-items-center'>
-            <ul className = { Mobile ? "nav-links-mobile" : "nav-links"} onClick={() =>setMobile(false)}>
+            <ul className={Mobile ? "nav-menu nav-links-mobile" : "nav-menu nav-links"}
+              ref={menu}>
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/about'>About us</Link></li>
               <li><Link to='/port'>Portfolio</Link></li>
@@ -25,9 +37,10 @@ function Header() {
                 <button className='contact-btn'><a>Contact us</a></button>
               </li>
             </ul>
-            <button className="d-flex align-itens-center" onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <ClearIcon sx={{display: {md:"none"}, zIndex: "modal", fontSize: "30px"}} /> : <MenuIcon
-                sx={{ display: {md: "none", fontSize: "30px"} }} />}
+            <button className="d-flex align-itens-center" onClick={handleClick}>
+              {Mobile ? <ClearIcon sx={{ display: { md: "none" }, zIndex: "modal", fontSize: "30px" }} /> :
+                <MenuIcon
+                  sx={{ display: { md: "none", fontSize: "30px" } }} />}
             </button>
           </div>
         </div>
